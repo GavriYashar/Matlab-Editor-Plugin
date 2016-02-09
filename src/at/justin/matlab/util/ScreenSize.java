707525e24cae6ed886de5e56e7d95adaa20dc011
GavriYashar <ajustin828@gmail.com>
2016-02-09 21:05:28 +0100

    added util to determine screensize on multimonitor environment

---
+++ src/at/justin/matlab/util/ScreenSize.java
@@ -0,0 +1,18 @@
+package at.justin.matlab.util;
+
+import java.awt.*;
+
+/**
+ * Created by Andreas Justin on 2016 - 02 - 09.
+ */
+public class ScreenSize {
+    private static final GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+
+    public static int getWidth() {
+        return gd.getDisplayMode().getWidth();
+    }
+
+    public static int getHeight() {
+        return gd.getDisplayMode().getHeight();
+    }
+}
