4ef5d7159ffe0f5028a61ed70131f36d6e77129c
GavriYashar <ajustin828@gmail.com>
2016-02-25 00:41:39 +0100

    added ctrl+f12 callback

--- src/at/justin/matlab/KeyReleasedHandler.java
+++ src/at/justin/matlab/KeyReleasedHandler.java
@@ -1,6 +1,7 @@
 package at.justin.matlab;
 
 import at.justin.matlab.clipboardStack.ClipboardStack;
+import at.justin.matlab.fileStructure.FileStructure;
 import com.mathworks.util.tree.Tree;
 import com.mathworks.widgets.text.mcode.MTree;
 import matlabcontrol.MatlabInvocationException;
@@ -35,16 +36,23 @@ public class KeyReleasedHandler {
             ClipboardStack.getInstance().setVisible(true);
         }
 
+        if (ctrlOnlyFlag && e.getKeyCode() == KeyEvent.VK_F12) {
+            FileStructure.getINSTANCE().populate(EditorWrapper.getInstance());
+            FileStructure.getINSTANCE().setVisible(true);
+        }
+
         if (ctrlShiftFlag && e.getKeyCode() == KeyEvent.VK_E) {
             MTree mTree = MTree.parse(EditorWrapper.getInstance().gae().getText());
 
-            Tree commentTree = mTree.findAsTree(MTree.NodeType.COMMENT, MTree.NodeType.BLOCK_COMMENT);
-            Tree functionTree = mTree.findAsTree(MTree.NodeType.FUNCTION, MTree.NodeType.CELL_TITLE, MTree.NodeType.PROPERTIES, MTree.NodeType.ENUMERATION, MTree.NodeType.EVENT);
+            Tree<MTree.Node> commentTree = mTree.findAsTree(MTree.NodeType.COMMENT, MTree.NodeType.BLOCK_COMMENT);
+            Tree<MTree.Node> functionTree = mTree.findAsTree(MTree.NodeType.FUNCTION, MTree.NodeType.CELL_TITLE, MTree.NodeType.PROPERTIES, MTree.NodeType.ENUMERATION, MTree.NodeType.EVENT);
+            Tree<MTree.Node> methodsTree = mTree.findAsTree(MTree.NodeType.METHODS);
 
             try {
                 Matlab.getInstance().proxyHolder.get().feval("assignin", "base", "mTree", mTree);
                 Matlab.getInstance().proxyHolder.get().feval("assignin", "base", "commentTree", commentTree);
                 Matlab.getInstance().proxyHolder.get().feval("assignin", "base", "functionTree", functionTree);
+                Matlab.getInstance().proxyHolder.get().feval("assignin", "base", "methodsTree", methodsTree);
             } catch (MatlabInvocationException ignored) { }
         }
 
