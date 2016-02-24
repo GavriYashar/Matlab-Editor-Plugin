05ef44a079053407bcd9637359dba2eadeb67297
GavriYashar <ajustin828@gmail.com>
2016-02-25 00:41:02 +0100

    improved node utils

--- src/at/justin/matlab/util/NodeUtils.java
+++ src/at/justin/matlab/util/NodeUtils.java
@@ -9,6 +9,27 @@ import com.mathworks.widgets.text.mcode.MTree;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * Get node Strings
+ * a Node may look like the following
+ * lvl   node                                     description
+ * 1     METHODS [12, 5] to [15, 7]               this is the whole method body
+ * 2      ATTRIBUTES [12, 13] to [12, 36],        method attributes like static, private etc.
+ * 3       ATTR [12, 14] to [0, 0],               introducing an attribute
+ * 4        ID (Static) [12, 14] to [0, 0],       attribute
+ * 3       ATTR [12, 29] to [0, 0],               introducing an attribute
+ * 4        ID (Hidden) [12, 22] to [0, 0],       attribute
+ * 4        ID (false) [12, 31] to [0, 0],        attribute flag
+ * 3       FUNCTION [13, 9] to [14, 11],          introducing a function
+ * 4        ETC [13, 24] to [0, 0]
+ * 5         ID (a) [13, 19] to [0, 0]            out arg1
+ * 5         ID (b) [13, 21] to [0, 0]            out arg2
+ * 5         ETC [13, 30] to [13, 43],
+ * 6          ID (asdf) [13, 26] to [0, 0],       function name
+ * 6          ID (i) [13, 31] to [0, 0],          in arg1
+ * 6          ID (want) [13, 34] to [0, 0],       in arg2
+ * 6          ID (sth) [13, 40] to [0, 0]         in arg3
+ */
 public final class NodeUtils {
     private NodeUtils() {
     }
@@ -31,24 +52,6 @@ public final class NodeUtils {
     }
 
     public static String getMethodHeader(final MTree.Node node) {
-        // may look like the following
-        // lvl   node                                     description
-        // 1     METHODS [12, 5] to [15, 7]               this is the whole method body
-        // 2      ATTRIBUTES [12, 13] to [12, 36],        method attributes like static, private etc.
-        // 3       ATTR [12, 14] to [0, 0],               introducing an attribute
-        // 4        ID (Static) [12, 14] to [0, 0],       attribute
-        // 3       ATTR [12, 29] to [0, 0],               introducing an attribute
-        // 4        ID (Hidden) [12, 22] to [0, 0],       attribute
-        // 4        ID (false) [12, 31] to [0, 0],        attribute flag
-        // 3       FUNCTION [13, 9] to [14, 11],          introducing a function
-        // 4        ETC [13, 24] to [0, 0]
-        // 5         ID (a) [13, 19] to [0, 0]            out arg1
-        // 5         ID (b) [13, 21] to [0, 0]            out arg2
-        // 5         ETC [13, 30] to [13, 43],
-        // 6          ID (asdf) [13, 26] to [0, 0],       function name
-        // 6          ID (i) [13, 31] to [0, 0],          in arg1
-        // 6          ID (want) [13, 34] to [0, 0],       in arg2
-        // 6          ID (sth) [13, 40] to [0, 0]         in arg3
         if (node.getType() != MTree.NodeType.METHODS) {
             throw new IllegalArgumentException("node has to be a MTree.NodeType.METHODS");
         }
@@ -56,29 +59,47 @@ public final class NodeUtils {
         stringBuilder.append("methods ");
 
         List<MTree.Node> methodNodes = node.getSubtree();
-        Iterator methodIterator = methodNodes.iterator();
         // find ATTRIBUTES node
-        while (methodIterator.hasNext()) {
-            MTree.Node methodNextNode = (MTree.Node) methodIterator.next();
+        for (MTree.Node methodNextNode : methodNodes) {
             if (methodNextNode.getType() == MTree.NodeType.ATTRIBUTES) {
-                EditorWrapper ew = EditorWrapper.getInstance();
-                int startPos = ew.lc2pos(methodNextNode.getStartLine(),methodNextNode.getStartColumn());
-                int endPos = ew.lc2pos(methodNextNode.getEndLine(),methodNextNode.getEndColumn());
-                stringBuilder.append(ew.getText(startPos,endPos));
+                stringBuilder.append(NodeUtils.getTextForNode(methodNextNode));
             }
         }
-
-        stringBuilder.append(")");
         return stringBuilder.toString();
     }
 
+    public static String getClassdef(final MTree.Node node) {
+        if (node.getType() != MTree.NodeType.CLASSDEF) {
+            throw new IllegalArgumentException("node has to be a MTree.NodeType.CLASSDEF");
+        }
+        List<MTree.Node> classNodes = node.getSubtree();
+        // find ID node in CEXPR node
+        for (MTree.Node nextClassNode : classNodes) {
+            if (nextClassNode.getType() == MTree.NodeType.CEXPR) {
+                List<MTree.Node> cexprNodes = nextClassNode.getSubtree();
+                for (MTree.Node nextCEXPRNode : cexprNodes) {
+                    if (nextCEXPRNode.getType() == MTree.NodeType.ID) {
+                        return "classdef " + nextCEXPRNode.getText();
+                    }
+                }
+            }
+        }
+        return "no class constructor found";
+    }
+
     public static String getFunctionInArgs(final MTree.Node node) {
+        if (node.getType() != MTree.NodeType.FUNCTION) {
+            throw new IllegalArgumentException("node has to be a MTree.NodeType.FUNCTION");
+        }
         Iterator inputIter = node.getInputArguments().iterator();
         return concatenateArgs(inputIter);
     }
 
     public static String getFunctionOutArgs(final MTree.Node node) {
-        Iterator outIter = node.getInputArguments().iterator();
+        if (node.getType() != MTree.NodeType.FUNCTION) {
+            throw new IllegalArgumentException("node has to be a MTree.NodeType.FUNCTION");
+        }
+        Iterator outIter = node.getOutputArguments().iterator();
         return concatenateArgs(outIter);
     }
 
@@ -86,9 +107,14 @@ public final class NodeUtils {
         String functionName = node.getFunctionName().getText();
         if (functionName != null) {
             if (withSignature) {
-                return "[" + getFunctionOutArgs(node) + "] = "
-                        + functionName
-                        + "(" + getFunctionInArgs(node) + ")";
+                StringBuilder stringBuilder = new StringBuilder();
+                String outArgs = getFunctionOutArgs(node);
+                String inArgs = getFunctionInArgs(node);
+
+                if (outArgs.length() > 0) {
+                    stringBuilder.append("[").append(outArgs).append("] = ");
+                }
+                return stringBuilder.append(functionName).append("(").append(inArgs).append(")").toString();
             } else {
                 return functionName;
             }
@@ -98,6 +124,9 @@ public final class NodeUtils {
     }
 
     public static String getCellName(final MTree.Node node) {
+        if (node.getType() != MTree.NodeType.CELL_TITLE) {
+            throw new IllegalArgumentException("node has to be a MTree.NodeType.CELL_TITLE");
+        }
         String sectionName = node.getText();
         if (sectionName.contains("%%")) {
             sectionName = sectionName.substring(sectionName.indexOf("%%") + 2);
@@ -106,4 +135,16 @@ public final class NodeUtils {
         sectionName = sectionName.trim();
         return sectionName.length() == 0 ? null : sectionName;
     }
+
+    public static String getTextForNode(final MTree.Node node) {
+        EditorWrapper ew = EditorWrapper.getInstance();
+        int startPos = ew.lc2pos(node.getStartLine(),node.getStartColumn());
+        int endPos = ew.lc2pos(node.getEndLine(),node.getEndColumn()+1);
+
+        System.out.println("start: " + startPos);
+        System.out.println("  end: " + endPos);
+
+        return ew.getText(startPos,endPos);
+    }
+
 }
