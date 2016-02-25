54d263691e949e38c317689602e5763589e9eade
GavriYashar <ajustin828@gmail.com>
2016-02-25 03:20:18 +0100

    dummy populater, selection callback, double click action, custom renderer

--- src/at/justin/matlab/fileStructure/FileStructure.java
+++ src/at/justin/matlab/fileStructure/FileStructure.java
@@ -11,14 +11,22 @@ import com.mathworks.util.tree.Tree;
 import com.mathworks.widgets.text.mcode.MTree;
 
 import javax.swing.*;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreeSelectionModel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileStructure extends UndecoratedFrame
 {
-    private JTree jTree;
     private static FileStructure INSTANCE;
+    private JTree jTree;
+    private ArrayList<MTree.Node> positionNodes = new ArrayList<>(10);
 
     public FileStructure()
     {
@@ -26,16 +34,18 @@ public class FileStructure extends UndecoratedFrame
         int height = ScreenSize.getHeight();
 
         setUndecorated(true);
-        setSize(300, 600);
+        setSize(500, 600);
         setLocation(width/2 - getWidth()/2,height/2 - getHeight()/2);
 
         //create the jTree by passing in the root node
         jTree = new JTree();
+        jTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
         JScrollPane scrollPane = new JScrollPane(jTree);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
         add(scrollPane);
+        setResizable(true);
         addListeners();
     }
 
@@ -43,6 +53,31 @@ public class FileStructure extends UndecoratedFrame
         jTree.addKeyListener(closeListener);
         jTree.addMouseListener(mlClick);
         jTree.addMouseMotionListener(mlMove);
+
+        jTree.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                super.mouseReleased(e);
+                if (e.getClickCount() > 1) {
+                    setVisible(false);
+                }
+            }
+        });
+        jTree.addTreeSelectionListener(new TreeSelectionListener() {
+            @Override
+            public void valueChanged(TreeSelectionEvent e) {
+                int index = jTree.getMaxSelectionRow()-1;
+                System.out.println("index = " + index);
+                if (index < 0) index = 0;
+                if (index > positionNodes.size()-1) {
+                    System.out.println("Selected Index " + index+1 + " is greater than size of positions " + positionNodes.size());
+                    return;
+                }
+                System.out.println(positionNodes.get(index));
+                System.out.println("going to line: " + positionNodes.get(index).getStartLine() + " index = " + index);
+                EditorWrapper.getInstance().goToLine(positionNodes.get(index).getStartLine());
+            }
+        });
     }
 
 
@@ -50,10 +85,11 @@ public class FileStructure extends UndecoratedFrame
         DefaultMutableTreeNode root = new DefaultMutableTreeNode(ew.getShortName());
         MTree mTree = MTree.parse(EditorWrapper.getInstance().gae().getText());
         Tree<MTree.Node> classdef = mTree.findAsTree(MTree.NodeType.CLASSDEF);
-        if (classdef != null) {
+        if (classdef.getChildCount(classdef.getRoot()) > 0) {
             root.add(forClass(mTree, classdef.getChild(classdef.getRoot(),0)));
         }
         jTree.setModel(new DefaultTreeModel(root));
+        jTree.setCellRenderer(new TreeRenderer());
         expandAll();
     }
 
@@ -61,14 +97,18 @@ public class FileStructure extends UndecoratedFrame
         DefaultMutableTreeNode classdefNode = new DefaultMutableTreeNode(NodeUtils.getClassdef(classdef));
         Tree<MTree.Node> methodsTree = mTree.findAsTree(MTree.NodeType.METHODS);
 
+        EditorWrapper ew = EditorWrapper.getInstance();
+        positionNodes.add(classdef);
         for (int i = 0; i < methodsTree.getChildCount(methodsTree.getRoot()); i++) {
             MTree.Node method = methodsTree.getChild(methodsTree.getRoot(), i);
             DefaultMutableTreeNode methodNode = new DefaultMutableTreeNode(NodeUtils.getMethodHeader(method));
 
+            positionNodes.add(method);
             List<MTree.Node> methodsSub = method.getSubtree();
-            for (int j = 0; j < methodsSub.size(); j++) {
-                if (methodsSub.get(j).getType() == MTree.NodeType.FUNCTION) {
-                    methodNode.add(new DefaultMutableTreeNode(NodeUtils.getFunctionHeader(methodsSub.get(j),true)));
+            for (MTree.Node methodSub : methodsSub) {
+                if (methodSub.getType() == MTree.NodeType.FUNCTION) {
+                    methodNode.add(new DefaultMutableTreeNode(NodeUtils.getFunctionHeader(methodSub, true)));
+                    positionNodes.add(methodSub);
                 }
             }
             classdefNode.add(methodNode);
@@ -97,5 +137,22 @@ public class FileStructure extends UndecoratedFrame
         if (INSTANCE != null) return INSTANCE;
         INSTANCE = new FileStructure();
         return INSTANCE;
+    }
+
+    void dummy() {
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode("classdef TEST");
+        DefaultMutableTreeNode sub1 = new DefaultMutableTreeNode("methods (static = true)");
+        DefaultMutableTreeNode sub2 = new DefaultMutableTreeNode("methods (Static = true, private)");
+
+        DefaultMutableTreeNode subsub1 = new DefaultMutableTreeNode("[asdf] = test(obj)");
+
+        sub1.add(subsub1);
+        sub2.add((MutableTreeNode) subsub1.clone());
+        sub2.add((MutableTreeNode) subsub1.clone());
+
+        root.add(sub1);
+        root.add(sub2);
+        jTree.setModel(new DefaultTreeModel(root));
+        jTree.setCellRenderer(new TreeRenderer());
     }
 }
