8f8dc961abe205689efd6a7c62b0de2409d44631
GavriYashar <ajustin828@gmail.com>
2016-07-22 08:56:02 +0200

    minor updates
    removing unused import, test updated

--- src/at/justin/matlab/EditorApp.java
+++ src/at/justin/matlab/EditorApp.java
@@ -15,7 +15,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 /**
--- src/at/justin/matlab/Test.java
+++ src/at/justin/matlab/Test.java
@@ -1,17 +1,30 @@
 package at.justin.matlab;
 
-import com.mathworks.matlab.api.editor.Editor;
-import com.mathworks.matlab.api.editor.EditorApplicationListener;
+import at.justin.matlab.fileStructure.FileStructure;
 import com.mathworks.mde.desk.MLDesktop;
-import com.mathworks.mde.editor.MatlabEditorApplication;
 
 import java.awt.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
  * Created by Gavri on 05.02.2016.
  */
 public class Test {
+    public static void main(String[] args) throws InterruptedException {
+        FileStructure fileStructure = new FileStructure();
+        fileStructure.setVisible(true);
+
+        DateFormat df = new SimpleDateFormat("HH:mm:ss.SSS");
+        Date dateObj = new Date();
+        while (fileStructure.isVisible()) {
+            Thread.sleep(500);
+            dateObj.setTime(System.currentTimeMillis());
+            System.out.println("running " + df.format(dateObj));
+        }
+        System.out.println("stopped");
+    }
     private static java.util.List<String> VALID_EDITOR_CLASSES = new ArrayList<String>() {{
             add("com.mathworks.widgets.desk.DTMaximizedPane");
             add("com.mathworks.widgets.desk.DTFloatingPane");
