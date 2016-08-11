fe57f655e985fe743a7df29245e2d2189108062d
GavriYashar <ajustin828@gmail.com>
2016-08-11 09:58:34 +0200

    reformatting, enter inserts text

--- src/at/justin/matlab/clipboardStack/ClipboardStack.java
+++ src/at/justin/matlab/clipboardStack/ClipboardStack.java
@@ -1,13 +1,16 @@
 package at.justin.matlab.clipboardStack;
 
 import at.justin.matlab.EditorWrapper;
-import at.justin.matlab.util.ScreenSize;
 import at.justin.matlab.gui.components.UndecoratedFrame;
+import at.justin.matlab.util.KeyStrokeUtil;
+import at.justin.matlab.util.ScreenSize;
 
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -15,17 +18,24 @@ import java.awt.event.MouseEvent;
  * Created by Andreas Justin on 2016 - 02 - 09.
  */
 public class ClipboardStack {
+    private static final int IFW = JComponent.WHEN_IN_FOCUSED_WINDOW;
+    private static final String ENTER_ACTION = "ENTER";
     private static ClipboardStack INSTANCE;
-    public final UndecoratedFrame undecoratedFrame = new UndecoratedFrame();
-    public JList jList;
+    private final UndecoratedFrame undecoratedFrame = new UndecoratedFrame();
+    private JList jList;
+    private JTextArea jTextArea;
     private DefaultListModel<String> stringListModel;
-    public JTextArea jTextArea;
-
     private String[] strings = new String[10];
 
     private ClipboardStack() {
         create();
         undecoratedFrame.setVisible(false);
+    }
+
+    public static ClipboardStack getInstance() {
+        if (INSTANCE != null) return INSTANCE;
+        INSTANCE = new ClipboardStack();
+        return INSTANCE;
     }
 
     public void add(final String string) {
@@ -39,27 +49,21 @@ public class ClipboardStack {
         String[] newStrings = new String[10];
         newStrings[0] = string;
         for (int i = 1; i < strings.length; i++) {
-            newStrings[i] = strings[i-1];
+            newStrings[i] = strings[i - 1];
         }
         strings = newStrings;
 
         stringListModel.removeAllElements();
-        for (String s: strings) {
+        for (String s : strings) {
             if (s == null) break;
-            if (s.length() > 50) s = s.substring(0,49);
+            if (s.length() > 50) s = s.substring(0, 49);
             stringListModel.addElement(s);
         }
     }
 
     public void setVisible(boolean visible) {
         undecoratedFrame.setVisible(visible);
-        undecoratedFrame.setAlwaysOnTop (visible);
-    }
-
-    public static ClipboardStack getInstance() {
-        if (INSTANCE != null) return INSTANCE;
-        INSTANCE = new ClipboardStack();
-        return INSTANCE;
+        undecoratedFrame.setAlwaysOnTop(visible);
     }
 
     private void create() {
@@ -68,7 +72,7 @@ public class ClipboardStack {
 
         undecoratedFrame.setUndecorated(true);
         undecoratedFrame.setSize(300, 600);
-        undecoratedFrame.setLocation(width/2 - undecoratedFrame.getWidth()/2,height/2 - undecoratedFrame.getHeight()/2);
+        undecoratedFrame.setLocation(width / 2 - undecoratedFrame.getWidth() / 2, height / 2 - undecoratedFrame.getHeight() / 2);
 
         jList = new JList();
         jList.setBackground(undecoratedFrame.getBackground());
@@ -76,7 +80,7 @@ public class ClipboardStack {
         jList.setModel(stringListModel);
         jTextArea = new JTextArea();
         jTextArea.setEditable(false);
-        jTextArea.setFont(new Font(Font.MONOSPACED,Font.PLAIN, 12));
+        jTextArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
 
         JScrollPane scrollPane = new JScrollPane(jTextArea);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
@@ -86,7 +90,7 @@ public class ClipboardStack {
         undecoratedFrame.setLayout(layout);
         undecoratedFrame.add(jList);
         undecoratedFrame.add(scrollPane);
-        jList.setBorder(BorderFactory.createLineBorder(new Color(0,0,0,0),10));
+        jList.setBorder(BorderFactory.createLineBorder(new Color(0, 0, 0, 0), 10));
 
         addListeners();
     }
@@ -102,8 +106,7 @@ public class ClipboardStack {
             public void mouseReleased(MouseEvent e) {
                 super.mouseReleased(e);
                 if (e.getClickCount() > 1) {
-                    EditorWrapper.getInstance().setSelectedTxt(strings[jList.getSelectedIndex()]);
-                    ClipboardStack.getInstance().setVisible(false);
+                    insertSelectedText();
                 }
             }
         };
@@ -119,5 +122,19 @@ public class ClipboardStack {
         });
 
         jList.addMouseListener(mlClick);
+
+        KeyStroke ksU = KeyStrokeUtil.getKeyStroke(KeyEvent.VK_ENTER);
+        jList.getInputMap(IFW).put(ksU, ENTER_ACTION);
+        jList.getActionMap().put(ENTER_ACTION, new AbstractAction(ENTER_ACTION) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                insertSelectedText();
+            }
+        });
+    }
+
+    private void insertSelectedText() {
+        EditorWrapper.getInstance().setSelectedTxt(strings[jList.getSelectedIndex()]);
+        ClipboardStack.getInstance().setVisible(false);
     }
 }
