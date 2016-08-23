5e16f49e5cb87a364c9406ab35a0d085c006939e
GavriYashar <ajustin828@gmail.com>
2016-08-23 15:39:12 +0200

    added prefsPanel
    placeholder for now

---
+++ src/at/justin/matlab/prefs/PrefsPanel.java
@@ -0,0 +1,39 @@
+package at.justin.matlab.prefs;
+
+import com.mathworks.mwswing.MJPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Andreas Justin on 2016-08-12.
+ */
+
+// import com.mathworks.mlwidgets.prefs.MATLABProductNodePrefsPanel;
+public class PrefsPanel extends MJPanel {
+    /**
+     * @param dim it seems matlab passes in Dimension in Constructor
+     */
+    public PrefsPanel(Dimension dim) {
+        this.setName("MatlabEditorPluginSettings");
+        this.setSize(dim);
+
+        this.add(new JLabel("YAY!"));
+    }
+
+    public PrefsPanel() {
+        this.setName("MatlabEditorPluginSettings");
+        this.setSize(new Dimension(100, 200));
+
+        this.add(new JLabel("YAY!"));
+    }
+
+    public static MJPanel createPrefsPanel() {
+        return new PrefsPanel();
+    }
+
+    public static void commitPrefsChanges(boolean save) {
+        if (save) {
+        }
+    }
+}
