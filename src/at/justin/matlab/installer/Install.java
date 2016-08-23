2c1c85d97d92a89d508d2a289b9b225c252674f7
GavriYashar <ajustin828@gmail.com>
2016-08-23 15:36:57 +0200

    added basic installer

---
+++ src/at/justin/matlab/installer/Install.java
@@ -0,0 +1,64 @@
+package at.justin.matlab.installer;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Andreas Justin on 2016-08-22.
+ */
+public class Install {
+    private Install() {
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        JFrame jf = new JFrame("Installing MEP");
+        jf.getContentPane().add(new JPanelInstall());
+        jf.setLocationRelativeTo(null);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.pack();
+        jf.setVisible(true);
+
+        DateFormat df = new SimpleDateFormat("HH:mm:ss.SSS");
+        Date dateObj = new Date();
+        while (jf.isVisible()) {
+            Thread.sleep(500);
+            dateObj.setTime(System.currentTimeMillis());
+            jf.setTitle("running " + df.format(dateObj));
+        }
+        System.exit(0);
+    }
+
+    public static File getFileOfClass() throws IOException {
+        try {
+            return new File(Install.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
+        } catch (URISyntaxException e) {
+            throw new IOException(e.toString());
+        }
+    }
+
+    public static File getJavaClassPathTxt() throws IOException {
+        File file = new File(System.getProperty("user.home") + "\\Documents\\MATLAB\\javaclasspath.txt");
+        if (!file.exists()) {
+            throw new IOException("no file on " + file);
+        }
+        return file;
+    }
+}
---
+++ src/at/justin/matlab/installer/JPanelInstall.java
@@ -0,0 +1,382 @@
+package at.justin.matlab.installer;
+
+import at.justin.matlab.util.FileUtils;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Created by Andreas Justin on 2016-08-22.
+ */
+public class JPanelInstall extends JPanel {
+    private final static Color INVALID = new Color(224, 125, 112);
+    private final static Color VALID = new Color(139, 255, 109);
+    final JFileChooser fc = new JFileChooser();
+    private File jarMEP = null;
+    private File jarMCTL = null;
+    private File txtJCP = null;
+    private File id = null;
+    private File jarMEPID = null;
+    private File jarMCTLID = null;
+
+    private JTextArea jtJAR = null;
+    private JTextArea jtJCP = null;
+    private JTextArea jtID = null;
+
+    public JPanelInstall() {
+        setLayout();
+    }
+
+    private void setLayout() {
+        setLayout(new GridBagLayout());
+        addPathSelectionPanel();
+        addButtons();
+    }
+
+    private void addPathSelectionPanel() {
+        File file = null;
+        try {
+            file = Install.getFileOfClass();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        {
+            // label for JAR files
+            GridBagConstraints cLabel = new GridBagConstraints();
+            cLabel.gridy = 0;
+            cLabel.gridx = 0;
+            cLabel.weightx = 1;
+            cLabel.insets = new Insets(10, 10, 0, 0);
+            cLabel.fill = GridBagConstraints.HORIZONTAL;
+            add(new JLabel("Location of MEP and matconsolectl .JAR-files"), cLabel);
+
+            // jar textfield
+            GridBagConstraints cJARPath = new GridBagConstraints();
+            cJARPath.gridy = 1;
+            cJARPath.gridx = 0;
+            cJARPath.weightx = 1;
+            cJARPath.insets = new Insets(0, 10, 0, 10);
+            cJARPath.fill = GridBagConstraints.HORIZONTAL;
+            jtJAR = new JTextArea(file.getParent(), 1, 50);
+            add(jtJAR, cJARPath);
+            searchJars(file.getParentFile());
+
+            // jar browser button
+            GridBagConstraints cJARPathBrowse = new GridBagConstraints();
+            cJARPathBrowse.gridy = 1;
+            cJARPathBrowse.gridx = 1;
+            cJARPathBrowse.weightx = 0.1;
+            cJARPathBrowse.insets = new Insets(0, 0, 0, 10);
+            final JButton jb = new JButton("...");
+            jb.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                    int returnVal = fc.showOpenDialog(jb);
+                    if (returnVal == JFileChooser.APPROVE_OPTION) {
+                        File file = fc.getSelectedFile();
+                        jtJAR.setText(file.toString());
+                        searchJars(file);
+                    }
+                }
+            });
+            add(jb, cJARPathBrowse);
+        }
+        {
+            // label for javaclasspath.txt
+            GridBagConstraints cLabel = new GridBagConstraints();
+            cLabel.gridy = 2;
+            cLabel.gridx = 0;
+            cLabel.weightx = 1;
+            cLabel.insets = new Insets(10, 10, 0, 0);
+            cLabel.fill = GridBagConstraints.HORIZONTAL;
+            add(new JLabel("Location of javaclasspath.txt"), cLabel);
+
+            // jcp textfield
+            File file2 = null;
+            try {
+                file2 = Install.getJavaClassPathTxt();
+                jtJCP = new JTextArea(file2.toString());
+                txtJCP = file2;
+            } catch (IOException ignored) {
+                jtJCP = new JTextArea();
+            }
+            GridBagConstraints cJCPPath = new GridBagConstraints();
+            cJCPPath.gridy = 3;
+            cJCPPath.gridx = 0;
+            cJCPPath.weightx = 1;
+            cJCPPath.insets = new Insets(0, 10, 0, 10);
+            cJCPPath.fill = GridBagConstraints.HORIZONTAL;
+            add(jtJCP, cJCPPath);
+
+            // jcp browser button
+            GridBagConstraints cJCPPathBrowse = new GridBagConstraints();
+            cJCPPathBrowse.gridy = 3;
+            cJCPPathBrowse.gridx = 1;
+            cJCPPathBrowse.weightx = 0.1;
+            cJCPPathBrowse.insets = new Insets(0, 0, 0, 10);
+            final JButton jb = new JButton("...");
+            jb.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                    int returnVal = fc.showOpenDialog(jb);
+
+                    if (returnVal == JFileChooser.APPROVE_OPTION) {
+                        File file = fc.getSelectedFile();
+                        if (file.getName().startsWith("javaclasspath")) {
+                            txtJCP = file;
+                        }
+                        jtJCP.setText(file.toString());
+                        checkJCPPath();
+                    }
+                }
+            });
+            add(jb, cJCPPathBrowse);
+        }
+        {
+            // label for installdir
+            GridBagConstraints cLabel = new GridBagConstraints();
+            cLabel.gridy = 4;
+            cLabel.gridx = 0;
+            cLabel.weightx = 1;
+            cLabel.insets = new Insets(10, 10, 0, 0);
+            cLabel.fill = GridBagConstraints.HORIZONTAL;
+            add(new JLabel("Installation directory"), cLabel);
+
+            GridBagConstraints cID = new GridBagConstraints();
+            cID.gridy = 5;
+            cID.gridx = 0;
+            cID.weightx = 1;
+            cID.insets = new Insets(0, 10, 0, 10);
+            cID.fill = GridBagConstraints.HORIZONTAL;
+            jtID = new JTextArea("", 1, 50);
+            add(jtID, cID);
+
+            // jcp browser button
+            GridBagConstraints cIDPathBrowse = new GridBagConstraints();
+            cIDPathBrowse.gridy = 5;
+            cIDPathBrowse.gridx = 1;
+            cIDPathBrowse.weightx = 0.1;
+            cIDPathBrowse.insets = new Insets(0, 0, 0, 10);
+            final JButton jb = new JButton("...");
+            jb.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                    int returnVal = fc.showOpenDialog(jb);
+
+                    if (returnVal == JFileChooser.APPROVE_OPTION) {
+                        File file = fc.getSelectedFile();
+                        id = new File(file.toString() + "\\MEP");
+                        jtID.setText(id.toString());
+                    }
+                }
+            });
+            add(jb, cIDPathBrowse);
+        }
+
+        checkJARPath();
+        checkJCPPath();
+    }
+
+    private void searchJars(File folder) {
+        File[] files = folder.listFiles();
+
+        for (File f : files) {
+            String s = f.getName();
+            if (s.startsWith("matconsolectl")) {
+                if (jarMCTL != null) {
+                    // if there are more than one files matching this, only one allowed
+                    jarMCTL = null;
+                    break;
+                }
+                jarMCTL = f;
+            }
+            if (s.startsWith("MEP")) {
+                if (jarMEP != null) {
+                    // if there are more than one files matching this, only one allowed
+                    jarMEP = null;
+                    break;
+                }
+                jarMEP = f;
+            }
+        }
+        checkJARPath();
+    }
+
+    private void addButtons() {
+        JPanel jp = new JPanel();
+        jp.setLayout(new GridBagLayout());
+
+        // install Button
+        GridBagConstraints cBI = new GridBagConstraints();
+        cBI.gridy = 0;
+        cBI.gridx = 0;
+        cBI.weightx = 1;
+        cBI.insets = new Insets(10, 10, 10, 0);
+        cBI.anchor = GridBagConstraints.LINE_START;
+        JButton jbi = new JButton("Install");
+        jp.add(jbi, cBI);
+
+        jbi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                doInstall();
+            }
+        });
+
+        // // uninstall Button
+        // GridBagConstraints cBU = new GridBagConstraints();
+        // cBU.gridy = 0;
+        // cBU.gridx = 1;
+        // cBU.weightx = 1;
+        // cBU.insets = new Insets(10, 10, 10, 0);
+        // cBU.anchor = GridBagConstraints.LINE_END;
+        // JButton jbu = new JButton("Uninstall");
+        // jp.add(jbu, cBU);
+
+        GridBagConstraints cp = new GridBagConstraints();
+        cp.gridy = 6;
+        cp.gridx = 0;
+        cp.weightx = 1;
+        cp.insets = new Insets(10, 10, 10, 10);
+        cp.fill = GridBagConstraints.HORIZONTAL;
+        add(jp, cp);
+    }
+
+    private void checkJCPPath() {
+        if (txtJCP == null) {
+            jtJCP.setBackground(INVALID);
+        } else {
+            jtJCP.setBackground(VALID);
+        }
+    }
+
+    private void checkJARPath() {
+        if (jarMCTL == null || jarMEP == null) {
+            jtJAR.setBackground(INVALID);
+        } else {
+            jtJAR.setBackground(VALID);
+        }
+    }
+
+    private void doInstall() {
+        if (jarMCTL == null || jarMEP == null || txtJCP == null) {
+            JOptionPane.showMessageDialog(
+                    new JFrame(""),
+                    "Please make sure that there is only one .jar file of both MEP_xxxxa and matconsolectl-v.v.v.jar in path.\nAlso make sure that the path to javaclasspath.txt is correct",
+                    "Invalid paths",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        if (id == null) {
+            JOptionPane.showMessageDialog(
+                    new JFrame(""),
+                    "No installation path selected",
+                    "Invalid installation path",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        id.mkdir();
+        copyFiles();
+        modifyJCPT();
+    }
+
+    private void modifyJCPT() {
+        boolean addMEP = true;
+        boolean addMCTL = true;
+        java.util.List<String> lines;
+        try {
+            lines = FileUtils.readFileToStringList(txtJCP);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(
+                    new JFrame(""),
+                    e.getMessage()
+                            + "\n\n"
+                            + "please modify javaclasspath.txt by hand and add both jars from installdirectory"
+                            + "\nstart Matlab and type 'edit javaclasspath.txt' and add both jars (full qualified name)"
+                            + "\nthen restart matlab",
+                    "unable to read javaclasspath.txt",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        for (String s : lines) {
+            if (!s.startsWith("#") && s.contains("MEP_")) {
+                addMEP = false;
+            }
+            if (!s.startsWith("#") && s.contains("matconsolectl")) {
+                addMCTL = false;
+            }
+            if (!addMCTL && !addMEP) {
+                break;
+            }
+        }
+        if (addMEP) {
+            appendJCPT(jarMEPID);
+        }
+        if (addMCTL) {
+            appendJCPT(jarMCTLID);
+        }
+        if (!addMCTL && !addMEP) {
+            JOptionPane.showMessageDialog(
+                    new JFrame(""),
+                    "already changed",
+                    "unable to read javaclasspath.txt",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void appendJCPT(File file) {
+        try {
+            FileUtils.appendFileText(txtJCP, file.toString());
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(
+                    new JFrame(""),
+                    e.getMessage(),
+                    "Uh Oh " + file.getName(),
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void copyFiles() {
+        {
+            // copy MEP
+            File f = new File(id + "\\" + jarMEP.getName());
+            FileUtils.copyFile(jarMEP, f);
+            jarMEPID = f;
+        }
+        {
+            // copy matconsolectl
+            File f = new File(id + "\\" + jarMCTL.getName());
+            FileUtils.copyFile(jarMCTL, f);
+            jarMCTLID = f;
+        }
+        {
+            // copy props
+            URL url = JPanelInstall.class.getResource("/properties/" + "DefaultProps.properties");
+            File fs = new File(url.getFile());
+            File ft1 = new File(id.getPath() + "\\" + "DefaultProps.properties");
+            File ft2 = new File(id.getPath() + "\\" + "CustomProps.properties");
+
+            try {
+                FileUtils.exportResource("/properties/DefaultProps.properties", ft1);
+                FileUtils.exportResource("/properties/DefaultProps.properties", ft2);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(
+                        new JFrame(""),
+                        e.getMessage(),
+                        "something went wrong, very very wrong",
+                        JOptionPane.ERROR_MESSAGE);
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
