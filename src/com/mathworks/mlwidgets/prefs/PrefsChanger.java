3cbdf981c96aca3a0597116d66922b0c321b8ece
GavriYashar <ajustin828@gmail.com>
2016-08-23 16:26:09 +0200

    added PrefsWelcome instead of PrefsPanel

--- src/com/mathworks/mlwidgets/prefs/PrefsChanger.java
+++ src/com/mathworks/mlwidgets/prefs/PrefsChanger.java
@@ -6,8 +6,7 @@ package com.mathworks.mlwidgets.prefs;
 public class PrefsChanger extends PrefsDialog {
     public static void addPrefs() throws ClassNotFoundException {
         PrefsDialog prefsDialog = (PrefsDialog) getMLPrefsDialog();
-        prefsDialog.showPrefsDialog();
-        prefsDialog.registerPanel("MEP", "at.justin.matlab.prefs.PrefsPanel", false);
+        prefsDialog.registerPanel("MEP", "at.justin.matlab.prefs.PrefsWelcome", false);
         prefsDialog.registerPanel("MEP.Settings", "at.justin.matlab.prefs.PrefsPanel", false);
     }
 }
