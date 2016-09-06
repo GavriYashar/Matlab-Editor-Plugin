19c329746d044b5c887679a3953e65ee5a2a3347
GavriYashar <ajustin828@gmail.com>
2016-09-06 13:03:55 +0200

    experimenting

--- src/at/justin/matlab/mesr/MESR.java
+++ src/at/justin/matlab/mesr/MESR.java
@@ -1,17 +1,40 @@
 package at.justin.matlab.mesr;
 
+import at.justin.matlab.EditorWrapper;
+
+import javax.swing.event.DocumentEvent;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Andreas Justin on 2016-09-06.
  */
 public class MESR {
-
     private static final MESR INSTANCE = new MESR();
+    private static EditorWrapper ew = EditorWrapper.getInstance();
+    private static final Pattern actionPattern = Pattern.compile("%\\w+(\\([\\w\\.,]+\\))?");
 
     private MESR() {
     }
 
     public static void doYourThing() {
-        System.out.println("MESR did it");
+        // int s = ew.getSelectionPositionStart();
+        // int e = ew.getSelectionPositionEnd();
+        // int[] lc = ew.pos2lc(s);
+        //
+        // String lineString = ew.getCurrentLineText();
+        // System.out.println(lineString);
+        // lineString = lineString.substring(0,lc[1]-1);
+        // System.out.println(lineString);
+        //
+        //
+        // Pattern actionPattern = Pattern.compile("%\\w+(\\([\\w\\.,]+\\))?%");
+        // Matcher matcher = actionPattern.matcher(lineString);
+        // if (matcher.find()) {
+        //     System.out.println(matcher.groupCount());
+        //     System.out.println(matcher.start());
+        //     System.out.println(matcher.end());
+        // }
     }
 
     public static MESR getInstance() {
