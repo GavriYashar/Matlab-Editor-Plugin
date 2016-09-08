d17a9758d7f15ec3630ea3871e67ead947f1e2e7
GavriYashar <ajustin828@gmail.com>
2016-09-08 11:32:27 +0200

    getting action

--- src/at/justin/matlab/mesr/MESR.java
+++ src/at/justin/matlab/mesr/MESR.java
@@ -2,7 +2,6 @@ package at.justin.matlab.mesr;
 
 import at.justin.matlab.EditorWrapper;
 
-import javax.swing.event.DocumentEvent;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,30 +10,32 @@ import java.util.regex.Pattern;
  */
 public class MESR {
     private static final MESR INSTANCE = new MESR();
-    private static EditorWrapper ew = EditorWrapper.getInstance();
     private static final Pattern actionPattern = Pattern.compile("%\\w+(\\([\\w\\.,]+\\))?");
+    private static EditorWrapper ew = EditorWrapper.getInstance();
 
     private MESR() {
     }
 
     public static void doYourThing() {
+        // System.out.println("MESR.doYourThing");
+        //
         // int s = ew.getSelectionPositionStart();
         // int e = ew.getSelectionPositionEnd();
         // int[] lc = ew.pos2lc(s);
+        // lc[1] -= 1;
         //
         // String lineString = ew.getCurrentLineText();
-        // System.out.println(lineString);
-        // lineString = lineString.substring(0,lc[1]-1);
-        // System.out.println(lineString);
-        //
-        //
-        // Pattern actionPattern = Pattern.compile("%\\w+(\\([\\w\\.,]+\\))?%");
+        // lineString = lineString.substring(0, lc[1]);
         // Matcher matcher = actionPattern.matcher(lineString);
-        // if (matcher.find()) {
-        //     System.out.println(matcher.groupCount());
-        //     System.out.println(matcher.start());
-        //     System.out.println(matcher.end());
+        //
+        // int[] se = {-1, -1};
+        // while (matcher.find()) {
+        //     se[0] = matcher.start();
+        //     se[1] = matcher.end() + 1;
         // }
+        // if (se[0] == -1 || se[1] != lc[1]) return; // only currently typed action
+        // String action = lineString.substring(se[0], se[1]);
+        // System.out.println(action);
     }
 
     public static MESR getInstance() {
