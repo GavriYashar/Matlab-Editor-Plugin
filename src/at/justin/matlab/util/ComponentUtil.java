de248f2813648db4cd68fd9cb5ee7125493c2d7a
GavriYashar <ajustin828@gmail.com>
2016-09-15 13:18:33 +0200

    added method to get components for editor or other components

--- src/at/justin/matlab/util/ComponentUtil.java
+++ src/at/justin/matlab/util/ComponentUtil.java
@@ -1,5 +1,9 @@
 package at.justin.matlab.util;
 
+import com.mathworks.matlab.api.editor.Editor;
+import com.mathworks.mde.editor.EditorSyntaxTextPane;
+import com.mathworks.widgets.editor.breakpoints.BreakpointView;
+
 import java.awt.*;
 import java.awt.List;
 import java.util.*;
@@ -29,5 +33,44 @@ public class ComponentUtil {
             }
         }
         return list;
+    }
+
+    public static java.util.List<Component> searchForComponentsRecursive(final Component component, String name) {
+        java.util.List<Component> foundComponents = new ArrayList<>(0);
+        if (!(component instanceof Container)) return foundComponents;
+        Component[] components = ((Container) component).getComponents();
+        for (Component c : components) {
+            if (c.getClass().toString().contains(name)) {
+                foundComponents.add(c);
+            } else if (c instanceof Container) {
+                java.util.List<Component> recFound = searchForComponentsRecursive(c, name);
+                foundComponents.addAll(recFound);
+            }
+        }
+        return foundComponents;
+    }
+
+    public static BreakpointView.Background getBreakPointViewForEditor(Editor editor) {
+        Component component = editor.getTextComponent().getParent().getParent();
+        java.util.List<Component> breakPointViews = ComponentUtil.searchForComponentsRecursive(component, "BreakpointView");
+        if (breakPointViews.size() > 1) {
+            System.out.println("Multiple BreakpointView found for editor \"" + editor.getLongName() + "\"");
+        }
+        if (breakPointViews.size() < 1) {
+            return null;
+        }
+        return (BreakpointView.Background) breakPointViews.get(0);
+    }
+
+    public static EditorSyntaxTextPane getEditorSyntaxTextPaneForEditor(Editor editor) {
+        Component[] components = editor.getTextComponent().getParent().getComponents();
+        EditorSyntaxTextPane editorSyntaxTextPane = null;
+        for (Component component : components) {
+            if (component instanceof EditorSyntaxTextPane) {
+                editorSyntaxTextPane = (EditorSyntaxTextPane) component;
+                break;
+            }
+        }
+        return editorSyntaxTextPane;
     }
 }
