bc2046b143e9a015adaadc804eab9b6a05e50a89
GavriYashar <ajustin828@gmail.com>
2016-10-12 13:08:46 +0200

    using KeyStrokes

--- src/at/justin/matlab/editor/EditorApp.java
+++ src/at/justin/matlab/editor/EditorApp.java
@@ -1,6 +1,7 @@
 package at.justin.matlab.editor;
 
 import at.justin.matlab.KeyReleasedHandler;
+import at.justin.matlab.Matlab;
 import at.justin.matlab.gui.autoDetailViewer.AutoDetailViewer;
 import at.justin.matlab.gui.bookmarks.Bookmarks;
 import at.justin.matlab.mepr.MEPR;
@@ -12,6 +13,7 @@ import com.mathworks.matlab.api.editor.EditorEvent;
 import com.mathworks.matlab.api.editor.EditorEventListener;
 import com.mathworks.mde.editor.EditorSyntaxTextPane;
 import com.mathworks.widgets.editor.breakpoints.BreakpointView;
+import matlabcontrol.MatlabInvocationException;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
@@ -120,6 +122,7 @@ public class EditorApp {
                 }
             }
 
+            addKeyStrokes(editorSyntaxTextPane);
             editorSyntaxTextPane.addKeyListener(KeyReleasedHandler.getKeyListener());
             editorSyntaxTextPane.getDocument().addDocumentListener(new DocumentListener() {
                 @Override
@@ -153,6 +156,33 @@ public class EditorApp {
         }
     }
 
+    private void addKeyStrokes(EditorSyntaxTextPane editorSyntaxTextPane) {
+        editorSyntaxTextPane.getInputMap(WF).put(MEPKeyStrokesE.KS_MEP_DEBUG.getKeyStroke(), "MEP_DEBUG");
+        editorSyntaxTextPane.getActionMap().put("MEP_DEBUG", MEPActionE.MEP_DEBUG.getAction());
+
+        editorSyntaxTextPane.getInputMap(WF).put(MEPKeyStrokesE.KS_MEP_SHOW_FILE_STRUCTURE.getKeyStroke(), "MEP_SHOW_FILE_STRUCTURE");
+        editorSyntaxTextPane.getActionMap().put("MEP_SHOW_FILE_STRUCTURE", MEPActionE.MEP_SHOW_FILE_STRUCTURE.getAction());
+
+        editorSyntaxTextPane.getInputMap(WF).put(MEPKeyStrokesE.KS_MEP_SHOW_COPY_CLIP_BOARD.getKeyStroke(), "MEP_SHOW_CLIP_BOARD_STACK");
+        editorSyntaxTextPane.getActionMap().put("MEP_SHOW_CLIP_BOARD_STACK", MEPActionE.MEP_SHOW_CLIP_BOARD_STACK.getAction());
+
+        editorSyntaxTextPane.getInputMap(WF).put(MEPKeyStrokesE.KS_MEP_COPY_CLIP_BOARD.getKeyStroke(), "MEP_COPY_CLIP_BOARD");
+        editorSyntaxTextPane.getActionMap().put("MEP_COPY_CLIP_BOARD", MEPActionE.MEP_COPY_CLIP_BOARD.getAction());
+
+        editorSyntaxTextPane.getInputMap(WF).put(MEPKeyStrokesE.KS_MEP_SHOW_BOOKMARKS.getKeyStroke(), "MEP_SHOW_BOOKMARKS");
+        editorSyntaxTextPane.getActionMap().put("MEP_SHOW_BOOKMARKS", MEPActionE.MEP_SHOW_BOOKMARKS.getAction());
+
+        editorSyntaxTextPane.getInputMap(WF).put(MEPKeyStrokesE.KS_MEP_DELETE_CURRENT_LINE.getKeyStroke(), "MEP_DELETE_CURRENT_LINE");
+        editorSyntaxTextPane.getActionMap().put("MEP_DELETE_CURRENT_LINE", MEPActionE.MEP_DELETE_CURRENT_LINE.getAction());
+
+        editorSyntaxTextPane.getInputMap(WF).put(MEPKeyStrokesE.KS_MEP_DUPLICATE_CURRENT_LINE.getKeyStroke(), "MEP_DUPLICATE_CURRENT_LINE");
+        editorSyntaxTextPane.getActionMap().put("MEP_DUPLICATE_CURRENT_LINE", MEPActionE.MEP_DUPLICATE_CURRENT_LINE.getAction());
+
+        // for some reason bookmarks don't work if editor is opened, while the others (actions) do
+        // editorSyntaxTextPane.getInputMap(WF).put(MEPKeyStrokesE.KS_MEP_BOOKMARK.getKeyStroke(), "MEP_BOOKMARK");
+        // editorSyntaxTextPane.getActionMap().put("MEP_BOOKMARK", MEPActionE.MEP_BOOKMARK.getAction());
+    }
+
     public void removeCallbacks() {
         List<Editor> openEditors = EditorWrapper.getMatlabEditorApplication().getOpenEditors();
         for (Editor editor : openEditors) {
@@ -173,119 +203,6 @@ public class EditorApp {
 
 //////////////////////////////////////////
 // UNUSED CODE
-
-// private static KeyStroke KS_FILESTRUCTURE = KeyStrokeUtil.getKeyStroke(KeyEvent.VK_F12, true, false, false);
-// private static KeyStroke KS_COPYCLIPBOARD = KeyStrokeUtil.getKeyStroke(KeyEvent.VK_C, true, false, false);
-// private static KeyStroke KS_SHOWCLIPBOARD = KeyStrokeUtil.getKeyStroke(KeyEvent.VK_V, true, true, false);
-// private static KeyStroke KS_DEBUG = KeyStrokeUtil.getKeyStroke(KeyEvent.VK_E, true, true, false);
-// private static KeyStroke KS_BOOKMARK = KeyStrokeUtil.getMatlabKeyStroke(MatlabKeyStrokesCommands.CTRL_PRESSED_F2);
-// private static KeyStroke KS_SHOWBOOKMARK = KeyStrokeUtil.getKeyStroke(
-//         KS_BOOKMARK.getKeyCode(),
-//         (KS_BOOKMARK.getModifiers() & KeyEvent.CTRL_DOWN_MASK) == KeyEvent.CTRL_DOWN_MASK,
-//         (KS_BOOKMARK.getModifiers() & KeyEvent.SHIFT_DOWN_MASK) != KeyEvent.SHIFT_DOWN_MASK,
-//         false);
-
-// JComponent jComponent = (JComponent) component;
-// jComponent.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK), "copyAction");
-// jComponent.getActionMap().put("copyAction", copyAction);
-
-// addKeyStrokes((EditorSyntaxTextPane) component);
-// private void addKeyStrokes(EditorSyntaxTextPane editorSyntaxTextPane) {
-//     editorSyntaxTextPane.getInputMap(WF).put(KS_COPYCLIPBOARD, "MEP_CopyClipBoard");
-//     editorSyntaxTextPane.getActionMap().put("MEP_CopyClipBoard", new AbstractAction("MEP_CopyClipBoard") {
-//         @Override
-//         public void actionPerformed(ActionEvent e) {
-//             KeyReleasedHandler.doCopyAction(e);
-//             System.out.println("MEP_CopyClipBoard");
-//         }
-//     });
-//     editorSyntaxTextPane.getInputMap(WF).put(KS_SHOWCLIPBOARD, "MEP_ShowCopyClipBoard");
-//     editorSyntaxTextPane.getActionMap().put("MEP_ShowCopyClipBoard", new AbstractAction("MEP_ShowCopyClipBoard") {
-//         @Override
-//         public void actionPerformed(ActionEvent e) {
-//             KeyReleasedHandler.showClipboardStack(e);
-//             System.out.println("MEP_ShowCopyClipBoard");
-//         }
-//     });
-//     editorSyntaxTextPane.getInputMap(WF).put(KS_FILESTRUCTURE, "MEP_ShowFileStructure");
-//     editorSyntaxTextPane.getActionMap().put("MEP_ShowFileStructure", new AbstractAction("MEP_ShowFileStructure") {
-//         @Override
-//         public void actionPerformed(ActionEvent e) {
-//             KeyReleasedHandler.showFileStructure(e);
-//             System.out.println("MEP_ShowFileStructure");
-//         }
-//     });
-//     editorSyntaxTextPane.getInputMap(WF).put(KS_DEBUG, "MEP_DEBUG");
-//     editorSyntaxTextPane.getActionMap().put("MEP_DEBUG", new AbstractAction("MEP_DEBUG") {
-//         @Override
-//         public void actionPerformed(ActionEvent e) {
-//             System.out.println("MEP_DEBUG");
-//             KeyReleasedHandler.DEBUG(e);
-//         }
-//     });
-// for some reason bookmakrs don't work if editor is opened, while the others do
-// editorSyntaxTextPane.getInputMap(WF).put(KS_BOOKMARK, "MEP_BOOKMARK");
-// editorSyntaxTextPane.getActionMap().put("MEP_BOOKMARK", new AbstractAction("MEP_BOOKMARK") {
-//     @Override
-//     public void actionPerformed(ActionEvent e) {
-//         System.out.println("MEP_BOOKMARK");
-//     }
-// });
-// editorSyntaxTextPane.getInputMap(WF).put(KS_SHOWBOOKMARK, "MEP_SHOWBOOKMARKS");
-// editorSyntaxTextPane.getActionMap().put("MEP_SHOWBOOKMARKS", new AbstractAction("MEP_SHOWBOOKMARKS") {
-//     @Override
-//     public void actionPerformed(ActionEvent e) {
-//         System.out.println("MEP_SHOWBOOKMARKS");
-//         KeyReleasedHandler.showBookmarksViewer(e);
-//     }
-// });
-// }
-
-//
-// add callbacks the old way
-// List<Component> list = Matlab.getInstance().getComponents("EditorSyntaxTextPane");
-//         for (Component component : list) {
-//                 KeyListener[] keyListeners = component.getKeyListeners();
-//                 if (Settings.getPropertyBoolean("verbose")) {
-//                 System.out.println("\n" + keyListeners.length + " keylisteners");
-//                 for (KeyListener keyListener : keyListeners) {
-//                 System.out.println(keyListener.toString());
-//                 }
-//                 }
-//
-//                 for (KeyListener keyListener1 : keyListeners) {
-//                 if (keyListener1.toString().equals(KeyReleasedHandler.getKeyListener().toString())) {
-//                 component.removeKeyListener(keyListener1);
-//              // this will assure that the new keylistener is added and the previous one is removed
-//                // while matlab is still running and the .jar is replaced
-//                 }
-//                 }
-//                 component.addKeyListener(KeyReleasedHandler.getKeyListener());
-//                 EditorSyntaxTextPane editorSyntaxTextPane = (EditorSyntaxTextPane) component;
-//                 editorSyntaxTextPane.getDocument().addDocumentListener(new DocumentListener() {
-// @Override
-// public void insertUpdate(DocumentEvent e) {
-//         Bookmarks.getInstance().adjustBookmarks(e, true);
-//         try {
-//         String insertString = e.getDocument().getText(e.getOffset(), e.getLength());
-//         if (insertString.equals("%")) MEPR.doYourThing();
-//         } catch (BadLocationException ignored) {
-//         ignored.printStackTrace();
-//         }
-//         }
-//
-// @Override
-// public void removeUpdate(DocumentEvent e) {
-//         Bookmarks.getInstance().adjustBookmarks(e, false);
-//         }
-//
-// @Override
-// public void changedUpdate(DocumentEvent e) {
-//         EditorWrapperActive.setIsDirty(true);
-//         }
-//         });
-//         }
-
 // old  breakpointview color
 // List<Component> list = Matlab.getInstance().getComponents("BreakpointView$2");
 // for (Component component : list) {
