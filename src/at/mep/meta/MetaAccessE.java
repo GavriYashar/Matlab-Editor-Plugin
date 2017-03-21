c46a78487e6671bf836901e822298ad9c438b8b8
GavriYashar <ajustin828@gmail.com>
2017-03-21 21:21:06 +0100

    introduced MetaAccessE

---
+++ src/at/mep/meta/MetaAccessE.java
@@ -0,0 +1,19 @@
+package at.mep.meta;
+
+/** Created by Gavri on 2017-03-21. */
+public enum MetaAccessE {
+    PRIVATE(0),
+    PROTECTED(1),
+    PUBLIC(2),
+    META(3);
+
+    private final int access;
+
+    MetaAccessE(int access) {
+        this.access = access;
+    }
+
+    public int getAccess() {
+        return access;
+    }
+}
