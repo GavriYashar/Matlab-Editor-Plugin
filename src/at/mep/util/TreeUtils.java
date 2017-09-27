3f54a07fc9a2d2611d4e98de4e758f7ee41803e0
GavriYashar <ajustin828@gmail.com>
2017-09-27 17:34:22 +0200

    cleanup, doku, added properties for filestructure
    thats what you get when the necessary port is closed

--- src/at/mep/util/TreeUtils.java
+++ src/at/mep/util/TreeUtils.java
@@ -1,24 +1,24 @@
 package at.mep.util;
 
-import at.mep.debug.Debug;
 import at.mep.gui.fileStructure.Node;
-import at.mep.meta.Meta;
 import at.mep.meta.MetaClass;
 import at.mep.meta.MetaMethod;
+import at.mep.meta.MetaProperty;
 import com.mathworks.util.tree.Tree;
 import com.mathworks.widgets.text.mcode.MTree;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /** Created by Gavri on 2017-04-28. */
 public class TreeUtils {
-
     public static Node toFileStructureNodeClass(MTree mTree, String nameRoot) {
         Tree<MTree.Node> treeClassdef = mTree.findAsTree(MTree.NodeType.CLASSDEF);
         if (treeClassdef.getChildCount(treeClassdef.getRoot()) < 1) {
             return null;
         }
 
+        // creates MetaObjectWrapper for Matlabs "meta.class"
         MetaClass metaClass;
         try {
             metaClass = MetaClass.getMatlabClass(nameRoot);
@@ -27,50 +27,21 @@ public class TreeUtils {
             return null;
         }
 
-        Tree<MTree.Node> methodsTree = mTree.findAsTree(MTree.NodeType.METHODS);
-        java.util.List<MTree.Node> methodNodes = new ArrayList<>(10);
-        for (int i = 0; i < methodsTree.getChildCount(methodsTree.getRoot()); i++) {
-            MTree.Node method = methodsTree.getChild(methodsTree.getRoot(), i);
-            Node methodNode = new Node(method);
-
-            java.util.List<MTree.Node> methodsSub = method.getSubtree();
-            for (MTree.Node methodSub : methodsSub) {
-                if (methodSub.getType() == MTree.NodeType.FUNCTION) {
-                    methodNodes.add(methodSub);
-                }
-            }
-        }
+        // create Class (root) node for FileStructure
         Node classDefNode = new Node(metaClass, mTree.getNode(0));
 
-        int counter = 0;
-        for (int i = 0; i < methodNodes.size(); i++) {
-            boolean wasInMethodNodes = false;
-            String nodeString = NodeUtils.getFunctionHeader(methodNodes.get(i), false);
-            for (MetaMethod m : metaClass.getMethods()) {
-                // inherited, see FileStructure inherited checkbox
-//                if (!inherited.isSelected()) {
-//                    if (!m.getDefiningClass().equals(fqn)) {
-//                        continue;
-//                    }
-//                }
+        // creates Method ("function") Nodes for FileStructure
+        Tree<MTree.Node> methodsTree = mTree.findAsTree(MTree.NodeType.FUNCTION);
+        java.util.List<MTree.Node> methodNodes = createNodesForClassDef(methodsTree, MTree.NodeType.FUNCTION);
 
-                Node method = null;
-                if (nodeString != null && nodeString.equals(m.getName())) {
-                    wasInMethodNodes = true;
-                    method = new Node(m, methodNodes.get(i));
-                }
-                if (method == null) continue;
-                classDefNode.add(method);
-                counter++;
-                if (counter == methodNodes.size()) break;
-            }
-            if (!wasInMethodNodes && Debug.isDebugEnabled()) {
-                MetaMethod m = new MetaMethod(methodNodes.get(i));
-                classDefNode.add(new Node(m, methodNodes.get(i)));
-                String msg = "i: " + i + " nodeString: " + nodeString + " text: " + methodNodes.get(i).getFunctionName().getText();
-                System.out.println(msg);
-            }
-        }
+        // creates Property Nodes for FileStructure
+        Tree<MTree.Node> propertiesTree = mTree.findAsTree(MTree.NodeType.PROPTYPEDECL);
+        java.util.List<MTree.Node> propertyNodes = createNodesForClassDef(propertiesTree, MTree.NodeType.PROPTYPEDECL);
+
+        // population of ClassDefNode
+        populateClassDefNodeWithProperties(classDefNode, metaClass, methodNodes, propertyNodes);
+        populateClassDefNodeWithMethods(classDefNode, metaClass, methodNodes);
+
         return classDefNode;
     }
 
@@ -93,5 +64,101 @@ public class TreeUtils {
             node.add(new Node(subNode));
         }
         return node;
+    }
+
+    private static java.util.List<MTree.Node> createNodesForClassDef(Tree<MTree.Node> tree, MTree.NodeType nodeType) {
+        java.util.List<MTree.Node> nodes = new ArrayList<>(10);
+        for (int i = 0; i < tree.getChildCount(tree.getRoot()); i++) {
+            MTree.Node mtNode = tree.getChild(tree.getRoot(), i);
+            java.util.List<MTree.Node> methodsSub = mtNode.getSubtree();
+            for (MTree.Node methodSub : methodsSub) {
+                if (methodSub.getType() == nodeType) {
+                    nodes.add(methodSub);
+                }
+            }
+        }
+        return nodes;
+    }
+
+    /**
+     * Helper method to create nodes to populate FileStructure
+     * @param classDefNode
+     * @param metaClass
+     * @param propertyNodes
+     */
+    private static void populateClassDefNodeWithProperties(Node classDefNode, MetaClass metaClass, java.util.List<MTree.Node> methodNodes, java.util.List<MTree.Node> propertyNodes) {
+        int counter = 0; // max: methodNodes.size()
+        for (int i = 0; i < propertyNodes.size(); i++) {
+            String nodeString = NodeUtils.getPropertyDecl(propertyNodes.get(i));
+            for (MetaProperty p : metaClass.getProperties()) {
+                Node property = null;
+                if (nodeString != null && nodeString.equals(p.getName())) {
+                    property = new Node(p, propertyNodes.get(i));
+                }
+                if (property == null) continue;
+
+                if (p.isHasGetter()) {
+                    Node node = findPropertySetGetMethod(methodNodes, p, "get.");
+                    classDefNode.add(node);
+                    property.add(node);
+                }
+                if (p.isHasSetter()) {
+                    Node node = findPropertySetGetMethod(methodNodes, p, "set.");
+                    classDefNode.add(node);
+                    property.add(node);
+                }
+                classDefNode.add(property);
+
+                counter++;
+                if (counter == propertyNodes.size()) break;
+            }
+        }
+    }
+
+    private static Node findPropertySetGetMethod(List<MTree.Node> methodNodes, MetaProperty p, String strGetSet) {
+        if ( !(strGetSet.equals("set.") || strGetSet.equals("get.")) ) {
+            throw new IllegalArgumentException("namePropertyMethod has to be equal to 'set.' or 'get.'");
+        }
+        for (MTree.Node mtNode : methodNodes) {
+            Node method = null;
+
+            String namePropSetGet = strGetSet + p.getName();
+            if (namePropSetGet.equals(mtNode.getFunctionName().getText())) {
+                method = new Node(p, mtNode);
+            }
+            if (method == null) continue;
+            return method;
+        }
+        return null;
+    }
+
+    /**
+     * Helper method to create nodes to populate FileStructure
+     * @param classDefNode
+     * @param metaClass
+     * @param methodNodes
+     */
+    private static void populateClassDefNodeWithMethods(Node classDefNode, MetaClass metaClass, java.util.List<MTree.Node> methodNodes) {
+        int counter = 0; // max: methodNodes.size()
+        for (int i = 0; i < methodNodes.size(); i++) {
+            String nodeString = NodeUtils.getFunctionHeader(methodNodes.get(i), false);
+            for (MetaMethod m : metaClass.getMethods()) {
+                // inherited, see FileStructure inherited checkbox
+                // if (!inherited.isSelected()) {
+                //     if (!m.getDefiningClass().equals(fqn)) {
+                //         continue;
+                //     }
+                // }
+
+                Node method = null;
+                if (nodeString != null && nodeString.equals(m.getName())) {
+                    method = new Node(m, methodNodes.get(i));
+                }
+                if (method == null) continue;
+                classDefNode.add(method);
+                counter++;
+                if (counter == methodNodes.size()) break;
+            }
+        }
     }
 }
