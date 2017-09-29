442d7bd65c350c30f7812431d383c10ffeaca99a
GavriYashar <ajustin828@gmail.com>
2017-09-29 18:53:11 +0200

    stringForMTreeNodeType refactored switch case to EMTreeNodeTypeString

--- src/at/mep/util/TreeUtilsV2.java
+++ src/at/mep/util/TreeUtilsV2.java
@@ -94,215 +94,10 @@ public class TreeUtilsV2 {
         return null;
     }
 
-
     public static String stringForMTreeNodeType(MTree.NodeType type) {
-        switch (type) {
-            case ERROR:
-                return "error";
-            case IF:
-                return "if";
-            case ELSE:
-                return "else";
-            case ELSEIF:
-                return "elseif";
-            case SWITCH:
-                return "switch";
-            case WHILE:
-                return "while";
-            case BREAK:
-                return "break";
-            case RETURN:
-                return "return";
-            case GLOBAL:
-                return "global";
-            case PERSISTENT:
-                return "persistent";
-            case TRY:
-                return "try";
-            case CATCH:
-                return "catch";
-            case CONTINUE:
-                return "continue";
-            case FUNCTION:
-                return "function";
-            case FOR:
-                return "for";
-            case PARFOR:
-                return "parfor";
-            case LEFT_PAREN:
-                return "(";
-            case RIGHT_PAREN:
-                return ")";
-            case LEFT_BRACKET:
-                return "[";
-            case RIGHT_BRACKET:
-                return "]";
-            case LEFT_CURLY_BRACE:
-                return "{";
-            case RIGHT_CURLY_BRACE:
-                return "}";
-            case AT_SIGN:
-                return "@";
-            case DOT_LEFT_PAREN:
-                return ".(";
-            case PLUS:
-                return "+";
-            case MINUS:
-                return "-";
-            case MULTIPLY:
-                return "*";
-            case DIVIDE:
-                return "/";
-            case LEFT_DIVIDE:
-                return "\\";
-            case EXPONENTIATION:
-                return "^";
-            case COLON:
-                return ":";
-            case DOT:
-                return ".";
-            case DOT_MULTIPLY:
-                return ".*";
-            case DOT_DIVIDE:
-                return "./";
-            case DOT_LEFT_DIVIDE:
-                return ".\\";
-            case DOT_EXPONENTIATION:
-                return ".^";
-            case AND:
-                return "&";
-            case OR:
-                return "|";
-            case ANDAND:
-                return "&&";
-            case OROR:
-                return "||";
-            case LT:
-                return "<";
-            case GT:
-                return ">";
-            case LE:
-                return "<=";
-            case GE:
-                return ">=";
-            case EQ:
-                return "=";
-            case NE:
-                return "~=";
-            case CASE:
-                return "case";
-            case OTHERWISE:
-                return "otherwise";
-            case DUAL:
-                return "dual";
-            case TRANS:
-                return "'";
-            case DOTTRANS:
-                return ".'";
-            case NOT:
-                return "~";
-            case ID:
-                return "id";
-            case INT:
-                return "int";
-            case DOUBLE:
-                return "double";
-            case STRING:
-                return "string";
-            case SEMI:
-                return ";";
-            case COMMA:
-                return ",";
-            case EOL:
-                return "EOL";
-            case BANG:
-                return "bang";
-            case END:
-                return "end";
-            case EQUALS:
-                return "==";
-            case CLASSDEF:
-                return "classdef";
-            case PROPERTIES:
-                return "properties";
-            case METHODS:
-                return "methods";
-            case EVENTS:
-                return "events";
-            case QUEST:
-                return "?";
-            case ENUMERATION:
-                return "enum";
-            case ERR:
-                return "error";
-            case CELL:
-                return "cell";
-            case SUBSCR:
-                return "subscr";
-            case CALL:
-                return "call";
-            case EXPR:
-                return "expr";
-            case PRINT_EXPR:
-                return "print_expr";
-            case ANON:
-                return "anon";
-            case ANONID:
-                return "anonid";
-            case DCALL:
-                return "dcall";
-            case JOIN:
-                return "join";
-            case LIST:
-                return "list";
-            case EVENT:
-                return "event";
-            case FIELD:
-                return "field";
-            case UMINUS:
-                return "uminus";
-            case UPLUS:
-                return "uplus";
-            case ATBASE:
-                return "@";
-            case CEXPR:
-                return "cexpr";
-            case ROW:
-                return "row";
-            case ATTR:
-                return "attr";
-            case ETC:
-                return "...";
-            case DISTFOR:
-                return "distfor";
-            case CELL_TITLE:
-                return "%%";
-            case COMMENT:
-                return "%";
-            case BLOCK_COMMENT:
-                return "%{";
-            case BLOCK_COMMENT_END:
-                return "%}";
-            case OLDFUN:
-                return "oldfun";
-            case PARENS:
-                return "parens";
-            case IFHEAD:
-                return "ifhead";
-            case PROTO:
-                return "proto";
-            case ATTRIBUTES:
-                return "attributes";
-            case SPMD:
-                return "spmd";
-            case PROPTYPEDECL:
-                return "proptypedecl";
-            case JAVA_NULL_NODE:
-                return "javaNullNode";
-            default:
-                throw new IllegalArgumentException("type: " + type + "not defined");
-        }
+        return EMTreeNodeTypeString.valueOf(type.name()).getDisplayString();
     }
+
 
     public static class AttributeHolder {
         private EAttributePropertyMethod attribute;
@@ -329,5 +124,119 @@ public class TreeUtilsV2 {
         private String name = ": NAME NOT SET";
         private String type = ": TYPE NOT DEFINED";
         private String validator = ": VALIDATORS NOT DEFINED";
+    }
+
+    private static enum EMTreeNodeTypeString {
+        ERROR("error"),
+        IF("if"),
+        ELSE("else"),
+        ELSEIF("elseif"),
+        SWITCH("switch"),
+        WHILE("while"),
+        BREAK("break"),
+        RETURN("return"),
+        GLOBAL("global"),
+        PERSISTENT("persistent"),
+        TRY("try"),
+        CATCH("catch"),
+        CONTINUE("continue"),
+        FUNCTION("function"),
+        FOR("for"),
+        PARFOR("parfor"),
+        LEFT_PAREN("("),
+        RIGHT_PAREN(")"),
+        LEFT_BRACKET("["),
+        RIGHT_BRACKET("]"),
+        LEFT_CURLY_BRACE("{"),
+        RIGHT_CURLY_BRACE("}"),
+        AT_SIGN("@"),
+        DOT_LEFT_PAREN(".("),
+        PLUS("+"),
+        MINUS("-"),
+        MULTIPLY("*"),
+        DIVIDE("/"),
+        LEFT_DIVIDE("\\"),
+        EXPONENTIATION("^"),
+        COLON(":"),
+        DOT("."),
+        DOT_MULTIPLY(".*"),
+        DOT_DIVIDE("./"),
+        DOT_LEFT_DIVIDE(".\\"),
+        DOT_EXPONENTIATION(".^"),
+        AND("&"),
+        OR("|"),
+        ANDAND("&&"),
+        OROR("||"),
+        LT("<"),
+        GT(">"),
+        LE("<="),
+        GE(">="),
+        EQ("="),
+        NE("~="),
+        CASE("case"),
+        OTHERWISE("otherwise"),
+        DUAL("dual"),
+        TRANS("'"),
+        DOTTRANS(".'"),
+        NOT("~"),
+        ID("id"),
+        INT("int"),
+        DOUBLE("double"),
+        STRING("string"),
+        SEMI(";"),
+        COMMA(","),
+        EOL("EOL"),
+        BANG("bang"),
+        END("end"),
+        EQUALS("=="),
+        CLASSDEF("classdef"),
+        PROPERTIES("properties"),
+        METHODS("methods"),
+        EVENTS("events"),
+        QUEST("?"),
+        ENUMERATION("enum"),
+        ERR("error"),
+        CELL("cell"),
+        SUBSCR("subscr"),
+        CALL("call"),
+        EXPR("expr"),
+        PRINT_EXPR("print_expr"),
+        ANON("anon"),
+        ANONID("anonid"),
+        DCALL("dcall"),
+        JOIN("join"),
+        LIST("list"),
+        EVENT("event"),
+        FIELD("field"),
+        UMINUS("uminus"),
+        UPLUS("uplus"),
+        ATBASE("@"),
+        CEXPR("cexpr"),
+        ROW("row"),
+        ATTR("attr"),
+        ETC("..."),
+        DISTFOR("distfor"),
+        CELL_TITLE("%%"),
+        COMMENT("%"),
+        BLOCK_COMMENT("%{"),
+        BLOCK_COMMENT_END("%}"),
+        OLDFUN("oldfun"),
+        PARENS("parens"),
+        IFHEAD("ifhead"),
+        PROTO("proto"),
+        ATTRIBUTES("attributes"),
+        SPMD("spmd"),
+        PROPTYPEDECL("proptypedecl"),
+        JAVA_NULL_NODE("javaNullNode");
+
+        private final String displayString;
+
+        EMTreeNodeTypeString(String displayString) {
+            this.displayString = displayString;
+        }
+
+        public String getDisplayString() {
+            return displayString;
+        }
     }
 }
