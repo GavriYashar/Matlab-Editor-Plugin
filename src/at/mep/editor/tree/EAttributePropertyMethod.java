a0d03ac9235f20c50e99a408ae416f3a771608f5
GavriYashar <ajustin828@gmail.com>
2017-09-29 15:18:55 +0200

    rename Attributes to EAttributePropertyMethod pt2

---
+++ src/at/mep/editor/tree/EAttributePropertyMethod.java
@@ -0,0 +1,37 @@
+package at.mep.editor.tree;
+
+import at.mep.meta.EMetaAccess;
+
+/** Created by Andreas Justin on 2017-09-29. */
+public enum EAttributePropertyMethod {
+    ABORTSET("AbortSet", EMetaAccess.FALSE),
+    ABSTRACT("Abstract", EMetaAccess.FALSE),
+    ACCESS("Access", EMetaAccess.PUBLIC),
+    CONSTANT("Constant", EMetaAccess.FALSE),
+    DEPENDENT("Dependent", EMetaAccess.FALSE),
+    GETACCESS("GetAccess", EMetaAccess.PUBLIC),
+    GETOBSERVABLE("GetObservable", EMetaAccess.FALSE),
+    HIDDEN("Hidden", EMetaAccess.FALSE),
+    NONCOPYABLE("NonCopyAble", EMetaAccess.FALSE),
+    SETACCESS("SetAccess", EMetaAccess.PUBLIC),
+    SETOBSERVABLE("SetObservable", EMetaAccess.FALSE),
+    TRANSIENT("Transient", EMetaAccess.FALSE),
+    SEALED("Sealed", EMetaAccess.FALSE),
+    STATIC("Static", EMetaAccess.FALSE);
+
+    private final String stringMatlab;
+    private final EMetaAccess defaultValue;
+
+    EAttributePropertyMethod(String stringMatlab, EMetaAccess defaultValue) {
+        this.stringMatlab = stringMatlab;
+        this.defaultValue = defaultValue;
+    }
+
+    public String getStringMatlab() {
+        return stringMatlab;
+    }
+
+    public EMetaAccess getDefaultValue() {
+        return defaultValue;
+    }
+}
