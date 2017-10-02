bfa55a5a79fcd6024a4b39dc8863dc122e24512e
GavriYashar <ajustin828@gmail.com>
2017-10-02 20:09:16 +0200

    removed unused property

--- src/at/mep/meta/Meta.java
+++ src/at/mep/meta/Meta.java
@@ -11,7 +11,6 @@ public abstract class Meta {
     protected String description = "";
     protected String detailedDescription = "";
     protected boolean isHidden = false;
-    private List<MTree.Node> attributes;
 
     public void setName(String name) {
         this.name = name;
