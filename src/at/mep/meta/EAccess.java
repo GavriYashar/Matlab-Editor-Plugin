6171ce8cd1f99b529b2278ff6c6de82e2621bef2
GavriYashar <ajustin828@gmail.com>
2017-10-03 18:26:31 +0200

    added method to get all enumerations as List<String>

--- src/at/mep/meta/EAccess.java
+++ src/at/mep/meta/EAccess.java
@@ -1,5 +1,9 @@
 package at.mep.meta;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /** Created by Gavri on 2017-03-21. */
 public enum EAccess {
     INVALID(-1),
@@ -14,6 +18,8 @@ public enum EAccess {
 
     private final int access;
 
+    private static List<String> names = new ArrayList<>(0);
+
     EAccess(int access) {
         this.access = access;
     }
@@ -24,5 +30,16 @@ public enum EAccess {
 
     public boolean convertBoolean() {
         return access == TRUE.access;
+    }
+
+    public static List<String> getNames() {
+        // public static List<String> getNames(Class<? extends Enum<?>> e)
+        if (names.size() == 0) {
+            names = Arrays.asList(
+                    Arrays.toString(EAccess.class.getEnumConstants())
+                            .replaceAll("^.|.$", "")
+                            .split(", "));
+        }
+        return names;
     }
 }
