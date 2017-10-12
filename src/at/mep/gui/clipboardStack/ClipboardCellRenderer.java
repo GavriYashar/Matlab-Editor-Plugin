36b4931e9661cf19383930c2eb30796d157fa9ac
GavriYashar <ajustin828@gmail.com>
2017-10-12 11:33:35 +0200

    renders strings starting with "%" (ignoring whitespaces) green

--- src/at/mep/gui/clipboardStack/ClipboardCellRenderer.java
+++ src/at/mep/gui/clipboardStack/ClipboardCellRenderer.java
@@ -1,5 +1,7 @@
 package at.mep.gui.clipboardStack;
 
+import at.mep.util.StringUtils;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,9 +11,19 @@ public class ClipboardCellRenderer extends DefaultListCellRenderer {
     public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
         Component c = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
         String s = (String) value;
-        if (s != null && s.length() > 50) {
+
+        if (s == null) {
+            return c;
+        }
+        if (s.length() > 50) {
             setText(s.substring(0, 49));
         }
+
+        String st = StringUtils.trimStart(s);
+        if (st.startsWith("%")) {
+            setForeground(new Color(0,128,0));
+        }
+
         return c;
     }
 }
