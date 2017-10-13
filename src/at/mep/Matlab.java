6c51b4bf9d55fda47416c87fe18fc1e630fbf250
GavriYashar <ajustin828@gmail.com>
2017-10-13 16:47:19 +0200

    fixed version numbers

--- src/at/mep/Matlab.java
+++ src/at/mep/Matlab.java
@@ -18,9 +18,10 @@ import java.util.regex.Pattern;
 
 /** Created by Andreas Justin on 2016-09-19. */
 public class Matlab {
-    public static final double R2014b = 8.7;
-    public static final double R2015a = 8.8;
-    public static final double R2015b = 8.9;
+    public static final double R2014a = 8.3;
+    public static final double R2014b = 8.4;
+    public static final double R2015a = 8.5;
+    public static final double R2015b = 8.6;
     public static final double R2016a = 9.0;
     public static final double R2016b = 9.1;
     public static final double R2017a = 9.2;
