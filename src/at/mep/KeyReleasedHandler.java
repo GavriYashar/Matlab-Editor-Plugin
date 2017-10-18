5be17f5f6fa4e7f649651037e9f24e2020126d70
GavriYashar <ajustin828@gmail.com>
2017-10-18 14:32:52 +0200

    deleted weird bookmarks thing

--- src/at/mep/KeyReleasedHandler.java
+++ src/at/mep/KeyReleasedHandler.java
@@ -1,6 +1,5 @@
 package at.mep;
 
-import at.mep.editor.EMEPAction;
 import at.mep.editor.EditorWrapper;
 import at.mep.mepr.MEPR;
 import at.mep.prefs.Settings;
@@ -8,7 +7,6 @@ import at.mep.util.KeyStrokeUtil;
 import com.mathworks.mde.cmdwin.XCmdWndView;
 import com.mathworks.mde.editor.EditorSyntaxTextPane;
 
-import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.regex.Matcher;
@@ -19,9 +17,6 @@ public class KeyReleasedHandler {
     private static String opEqString;
     private static Pattern opEqPattern = Pattern.compile("[\\+]"); // "[\\+\\-\\*/]"
     private static Pattern opEqLeftArgPattern = Pattern.compile("(\\s*)(.*?)(?=\\s*[\\+\\-\\*/]{2})");
-    private static boolean ctrlf2 = false; // if ctrl is released before F2. there may be a better way to fix the keyevent issue
-    private static KeyStroke KS_BOOKMARK;
-    private static KeyStroke KS_SHOWBOOKARKS;
     private static final KeyListener keyListener = new KeyListener() {
         @Override
         public void keyTyped(KeyEvent e) {
@@ -38,7 +33,6 @@ public class KeyReleasedHandler {
             if (Settings.getPropertyBoolean("enableDoubleOperator")) {
                 KeyReleasedHandler.doOperatorThing(e);
             }
-            KeyReleasedHandler.doBookmarkThing(e);
         }
 
         /**
@@ -80,32 +74,6 @@ public class KeyReleasedHandler {
                     && Settings.getPropertyBoolean("feature.enableReplacements")) {
                 MEPR.doYourThing();
             }
-
-            if (KS_BOOKMARK == null) {
-                KS_BOOKMARK = KeyStrokeUtil.getMatlabKeyStroke(EMatlabKeyStrokesCommands.CTRL_PRESSED_F2);
-            }
-            if (KS_BOOKMARK != null) {
-                if (KS_SHOWBOOKARKS == null) {
-                    KS_SHOWBOOKARKS = KeyStrokeUtil.getKeyStroke(
-                            KS_BOOKMARK.getKeyCode(),
-                            (KS_BOOKMARK.getModifiers() & KeyEvent.CTRL_DOWN_MASK) == KeyEvent.CTRL_DOWN_MASK,
-                            (KS_BOOKMARK.getModifiers() & KeyEvent.SHIFT_DOWN_MASK) != KeyEvent.SHIFT_DOWN_MASK,
-                            false,
-                            false);
-                }
-
-                if (KS_BOOKMARK.getModifiers() == mod && KS_BOOKMARK.getKeyCode() == e.getKeyCode())
-                    ctrlf2 = true;
-                    // Doing the bookmarks here is error prone and won't work correctly.
-                    // this function ist called every time (afaik) before Matlab's keyRelease
-                else if (KS_SHOWBOOKARKS.getModifiers() == mod && KS_SHOWBOOKARKS.getKeyCode() == e.getKeyCode())
-                    EMEPAction.MEP_SHOW_BOOKMARKS.getAction().actionPerformed(null);
-            } else if (Settings.issue57DisplayMessage) {
-                Settings.issue57DisplayMessage = false;
-                // FIXME: i could try to fix it to just manually assign it to CTRL+F2
-                //        but this causes other problems, if the user changed the shortcut.
-                System.out.println("Issue #57 is happening right now.");
-            }
         }
     }
 
@@ -133,12 +101,5 @@ public class KeyReleasedHandler {
             opEqString = "";
         }
         operatorEqualsThing(e);
-    }
-
-    private static void doBookmarkThing(KeyEvent e) {
-        if (ctrlf2) {
-            ctrlf2 = false;
-            EMEPAction.MEP_BOOKMARK.getAction().actionPerformed(null);
-        }
     }
 }
