4a9c6d1cb8e4c720d5877762f318edec6d321690
GavriYashar <ajustin828@gmail.com>
2018-01-22 13:47:22 +0100

    disabled custom contextmenu in editor

--- src/at/mep/gui/ContextMenu.java
+++ src/at/mep/gui/ContextMenu.java
@@ -1,10 +1,20 @@
 package at.mep.gui;
 
+import at.mep.editor.EditorWrapper;
+import at.mep.editor.tree.MFile;
+import at.mep.mepr.MEPR;
+import at.mep.prefs.Settings;
+import at.mep.util.TreeUtilsV2;
 import at.mep.workspace.WorkspaceWrapper;
 import com.mathworks.matlab.api.editor.Editor;
 import com.mathworks.mlwidgets.workspace.WorkspaceTable;
+import com.mathworks.mwswing.MJMenu;
+import com.mathworks.mwswing.MJMenuItem;
+import com.mathworks.mwswing.MJPopupMenu;
+import com.mathworks.widgets.text.mcode.MTree;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.util.List;
 
@@ -12,37 +22,31 @@ import java.util.List;
 public class ContextMenu {
 
     public static void contribute(Editor editor) {
-        return;
-        /*
+        if (true || !Settings.getPropertyBoolean("feature.enableReplacements")) {
+             return;
+        }
+
         MJPopupMenu cm = EditorWrapper.getContextMenu(editor);
+        Component[] components = cm.getComponents();
+        // setVisible(false) will lose the "fix" functionality e.g.: replace sth with ~
         cm.setVisible(false);
 
-        MJMenu mjmMEP = new MJMenu("MEP");
-
+        MJMenu mjmMEP = new MJMenu("MEP Insert");
         {
             MJMenuItem mjMenuItem = new MJMenuItem(new AbstractAction() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    System.out.println("yay1");
+                    EditorWrapper.insertTextAtPos("%rev%", EditorWrapper.getCaretPosition());
+                    MEPR.doYourThing();
                 }
             });
-            mjMenuItem.setText("text1");
+            mjMenuItem.setText("Revision Line");
             mjmMEP.add(mjMenuItem);
         }
-        {
-            MJMenuItem mjMenuItem = new MJMenuItem(new AbstractAction() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    System.out.println("yay2");
-                }
-            });
-            mjMenuItem.setText("text2");
-            mjmMEP.add(mjMenuItem);
-        }
+        contributeEditorClass(mjmMEP);
 
         cm.add(mjmMEP);
         cm.setVisible(true);
-        */
     }
 
     public static void contribute(WorkspaceTable workspaceTable) {
@@ -69,5 +73,32 @@ public class ContextMenu {
         cm.add(jMenuItem);
 
         cm.setVisible(true);
+    }
+
+    private static void contributeEditorClass(MJMenu mjMenu) {
+        MTree mTree = EditorWrapper.getMTreeFast(EditorWrapper.getActiveEditor());
+        if (TreeUtilsV2.getFileType(mTree) != TreeUtilsV2.FileType.ClassDefinitionFile) {
+            // only add if active editor is a ClassDefinitionFile
+            return;
+        }
+
+        {
+            MJMenuItem mjMenuItem = new MJMenuItem(new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    MFile mFile = MFile.construct(EditorWrapper.getActiveEditor());
+                    MFile.ClassDef classDef = mFile.getClassDefs().get(0);
+                    List<MFile.ClassDef.Properties> propertiesList = classDef.getProperties();
+                    
+                    JOptionPane.showMessageDialog(
+                            new JFrame(""),
+                            "Test message: " + mFile.getName(),
+                            "test title",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+            });
+            mjMenuItem.setText("Getter / Setter");
+            mjMenu.add(mjMenuItem);
+        }
     }
 }
