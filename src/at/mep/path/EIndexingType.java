583335b9ccf3a6d84c20db2582bc9225c79ceaec
GavriYashar <gavriyashargithub@gmail.com>
2018-03-11 20:58:40 +0100

    added EIndexingType

---
+++ src/at/mep/path/EIndexingType.java
@@ -0,0 +1,23 @@
+package at.mep.path;
+
+
+/**
+ * dynamic: index gets updated as needed.
+ * classes: does a scan for classes in matlab's search path, index also gets updated as needed
+ * full: does a full scan for .m files in matlabs search path, index also gets updated as needed
+ */
+public enum EIndexingType{
+    DYNAMIC (0),
+    CLASSES (1),
+    FULL (2);
+
+    private int indexingType;
+
+    EIndexingType(int indexingType) {
+        this.indexingType = indexingType;
+    }
+
+    public int getIndexingType() {
+        return indexingType;
+    }
+}
