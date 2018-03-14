0538af0320c070c4128a8e13649f2392cf921f40
GavriYashar <gavriyashargithub@gmail.com>
2018-03-14 22:24:32 +0100

    experimenting with docking

--- src/at/mep/gui/components/DockableFrameTest.java
+++ src/at/mep/gui/components/DockableFrameTest.java
@@ -1,18 +1,40 @@
 package at.mep.gui.components;
 
-import com.mathworks.mde.desk.MLDesktop;
 import com.mathworks.widgets.desk.DTClientBase;
 
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * +----------+----------+-------------+---------------------------------------------------+
+ * | Scenario | restore  | calls       | scenario description                              |
+ * |          | position | constructor |                                                   |
+ * +----------+----------+-------------+---------------------------------------------------+
+ * |     1    |     -    |      -      |  DFT was either never added or has been removed   |
+ * +----------+----------+-------------+---------------------------------------------------+
+ * |     2    |     x    |      x      |  DFT was added floating                           |
+ * +----------+----------+-------------+---------------------------------------------------+
+ * |     3    |     x    |      x      |  DFT was added docked as own panel                |
+ * +----------+----------+-------------+---------------------------------------------------+
+ * |     4    |     x    |      -      |  DFT was added as hidden tab                      |
+ * |          |          |      x      |  calls constructor upon activation                |
+ * +----------+----------+-------------+---------------------------------------------------+
+ * |     5    |     x    |      x      |  DFT was added as active tab                      |
+ * +----------+----------+-------------+---------------------------------------------------+
+ *
+ */
 public class DockableFrameTest extends DTClientBase {
-    public DockableFrameTest() {
+    private static final DockableFrameTest instance = new DockableFrameTest();
+    private DockableFrameTest() {
         this.setLayout(new BorderLayout());
         JLabel jLabel = new JLabel("Yay!");
         jLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
         this.add(jLabel, "Center");
 
-        MLDesktop.getInstance().addClient(this, "Test - Yay!");
+        System.out.println("MLDesktop.getInstance().addClient(this, \"Test - Yay!\"");
+    }
+
+    public static DockableFrameTest getInstance() {
+        return instance;
     }
 }
