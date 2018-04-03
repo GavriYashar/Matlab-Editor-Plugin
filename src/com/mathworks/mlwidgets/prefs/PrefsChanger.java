c6f857239e7661b364193260bd7ca4e2de11ed10
GavriYashar <gavriyashargithub@gmail.com>
2018-04-04 00:57:16 +0200

    removed unnecessary information

--- src/com/mathworks/mlwidgets/prefs/PrefsChanger.java
+++ src/com/mathworks/mlwidgets/prefs/PrefsChanger.java
@@ -1,11 +1,7 @@
 package com.mathworks.mlwidgets.prefs;
 
 import at.mep.Start;
-import at.mep.installer.Install;
-import at.mep.prefs.PrefsWindow;
 import at.mep.prefs.Settings;
-
-import java.io.IOException;
 
 /** Created by Andreas Justin on 2016-08-23. */
 public class PrefsChanger extends PrefsDialog {
@@ -15,16 +11,8 @@ public class PrefsChanger extends PrefsDialog {
             prefsDialog.registerPanel("MEP", "at.mep.prefs.PrefsWelcome", false);
             prefsDialog.registerPanel("MEP.Settings", "at.mep.prefs.PrefsPanel", false);
             Settings.setPropertyBoolean("jar.isOnStaticPath", true);
-        } catch (IllegalAccessError e) {
-            e.printStackTrace();
-            System.out.println();
-            System.out.println("MEP is not in Static path. Please Run the jar file or add MEP to javaclasspath.txt");
-            try {
-                System.out.println(Install.getJarFile().toString());
-            } catch (IOException ignored) {
-            }
-            System.out.println();
-            System.out.println("with this (below) you can still access a property editor, if you don't want to change this");
+        } catch (IllegalAccessError ignored) {
+            System.out.println("MEP is on Dynamic Path: with the following line you can still access the property editor");
             System.out.println(Start.class.getName() + ".openPrefsPanel();");
             System.out.println();
             Settings.setPropertyBoolean("jar.isOnStaticPath", false);
